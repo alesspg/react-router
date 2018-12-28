1054a61a59e3b263282aa82a891138e92b7719df
Alessandra De La Pompa <a.delapompag@gmail.com>
2018-12-28 16:44:08 -0500

    donegit add .

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './CSS/index.css';
 
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
+import Nav from './components/Nav';
 
 // your code goes here
 class Home extends Component{
   render(){
     return(
-      // your code goes here
-      <div>
-        test
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' component={Nav} >Home</Route>
+          <Route path="/happy" component={Happy}>Happy</Route>
+          <Route path="/sleepy" component={Sleepy}>Sleepy</Route>
+          <Route path='/guilty' component={Guilty}>Guilty</Route>
+        </div>
+      </BrowserRouter>
     )
   }
 }
